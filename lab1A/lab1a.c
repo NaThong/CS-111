cc13fa461215788bd29920f3c9f4e9b6fa9d117e
Jeffrey Chan <jeffreyc@lnxsrv09.seas.ucla.edu>
2017-04-17 23:09:55 -0700

    final revision lab1A

--- lab1A/lab1a.c
+++ lab1A/lab1a.c
@@ -8,12 +8,16 @@
 #include<unistd.h>
 #include<termios.h>
 #include<poll.h>
+#include<signal.h>
+#include<sys/wait.h>
+#include<sys/types.h>
 
 // struct to hold saved terminal attributes
 struct termios savedAttributes;
 
 // buffer to hold read characters
-char buffer;
+int BUFFER_SIZE = 1024;
+char extraBuffer[1024];
 
 // int arrays to hold pipes between processes
 int pipeToChild[2];
@@ -22,8 +26,23 @@ int pipeToParent[2];
 // process id for when forking process
 pid_t pid;
 
+int shellFlag; // flag for shell option
+
 void resetInputMode() {
     tcsetattr(STDIN_FILENO, TCSANOW, &savedAttributes);
+    if (shellFlag) {
+	int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            fprintf(stderr, "error: waitpid failed");
+            exit(EXIT_FAILURE);
+        }
+        if (WIFEXITED(status)) {
+            const int es = WEXITSTATUS(status);
+            const int ss = WTERMSIG(status);
+            fprintf(stderr, "SHELL EXIT SIGNAL=%d STATUS=%d\n", ss, es);
+            exit(0);
+        }
+    }
 }
 
 void setInputMode() {
@@ -50,6 +69,15 @@ void setInputMode() {
     }
 }
 
+void signalHandler(int signalNumber) {
+    if (shellFlag && signalNumber == SIGINT) {
+	kill(pid, SIGINT);
+    }
+    if (signalNumber == SIGPIPE) {
+	exit(0);
+    }
+}
+
 void createPipe(int pipeHolder[2]) {
     if (pipe(pipeHolder) == -1) {
 	fprintf(stderr, "error: error in creating pipe");
@@ -57,58 +85,88 @@ void createPipe(int pipeHolder[2]) {
     }
 }
 
-void closeAllPipes() {
-    close(pipeToChild[0]);
-    close(pipeToChild[1]);
-    close(pipeToParent[0]);
-    close(pipeToParent[1]);
-}
-
-void readWriteEcho(int fd1, int fd2) {
+void readWriteShell(int fd1, int fd2) {
     struct pollfd pollfdArray[2]; // array of pollfd strctures
     pollfdArray[0].fd = fd1; // first pollfd describes keyboard
-    pollfdArray[1].fd = pipeToParent[0]; // second pollfd describes pipe outputi
+    pollfdArray[1].fd = pipeToParent[0]; // second pollfd describes pipe output
     pollfdArray[0].events = POLLIN | POLLHUP | POLLERR;
     pollfdArray[1].events = POLLIN | POLLHUP | POLLERR;
 
     int returnValue;
+    char buffer;
 
     while (1) {
+	// do a polli and check for errors
 	returnValue = poll(pollfdArray, 2, 0);
-	if (returnValue < 0) {
+	/*if (returnValue < 0) {
 	    fprintf(stderr, "error: error while polling\n");
 	    exit(1);
+	}*/
+	
+	// if keyboard pollfd revents has POLLIN (has input to read)
+	if ((pollfdArray[0].revents & POLLIN)) {
+	    int bytesRead = read(fd1, &buffer, sizeof(char)); // read from keyboard
+	    if (buffer == '\r' || buffer == '\n') {
+		char tempBuffer[2] = {'\r', '\n'};
+		char shellBuffer[1] = {'\n'};
+		write(fd2, &tempBuffer, 2*sizeof(char));
+		write(pipeToChild[1], &shellBuffer, sizeof(char));
+		continue;
+	    }
+	    else if (buffer == 0x04) {
+		close(pipeToChild[1]);
+	    }
+	    else if (buffer == 0x03) {
+	    	kill(pid, SIGINT);
+	    }
+	    else {
+	        write(fd2, &buffer, sizeof(char));
+	        write(pipeToChild[1], &buffer, sizeof(char));
+	    }
 	}
-		
-    }
 
-/*
-    while (read(0, &buffer, sizeof(char))) {
+	// if shell pollfd has POLLIN (has output to read)
+	if ((pollfdArray[1].revents & POLLIN)) {
+	    int bytesRead = read(pipeToParent[0], &buffer, sizeof(char)); // read from shell pipe
+	    if (buffer == '\n') {
+		char tempBuffer[2] = {'\r', '\n'};
+		write(1, &tempBuffer, 2*sizeof(char));
+		continue;
+	    }
+	    else {
+		write(1, &buffer, sizeof(char));
+	    }
+	}
+
+	if ((pollfdArray[1].revents & (POLLHUP | POLLERR))) {
+	    exit(0);    
+	}
+    }
+}
+
+void readWrite(fd1, fd2) {
+    char buffer2;
+    while (read(0, &buffer2, sizeof(char))) {
     	// if receive ^D, exit
-	if (buffer == '\004') {
+	if (buffer2 == '\004') {
 	    exit(0);
 	}
 	// receiving \r or \n
-	else if (buffer == '\r' || buffer == '\n') {
+	else if (buffer2 == '\r' || buffer2 == '\n') {
 	    char tempBuffer[2] = {'\r', '\n'};
-	    char tempShellBuffer[1] = {'\n'};
-	    // should go to shell as <lf>
-	    write(pipeToChild[1], &tempShellBuffer, sizeof(char));
 	    // should echo as <cr><lf>
 	    write(1, &tempBuffer, 2*sizeof(char));
 	    continue;
 	}
 	else {
-	    write(pipeToChild[1], &buffer, sizeof(char));
-	    write(1, &buffer, sizeof(char));
+	    write(1, &buffer2, sizeof(char));
 	}
-    }*/
+    }
 }
 
 int main(int argc, char **argv) {
 
     int option = 0; // used to hold option
-    int shellFlag = 0; // flag for shell option
 
     // arguments that this program recognizes
     static struct option options[] = {
@@ -118,7 +176,11 @@ int main(int argc, char **argv) {
 
     while ((option = getopt_long(argc, argv, "s", options, NULL)) != -1) {
 	switch (option) {
-	    case 's': shellFlag = 1; break;
+	    case 's':
+		signal(SIGINT, signalHandler);
+		signal(SIGPIPE, signalHandler); 
+		shellFlag = 1;
+		break;
 	    default:
 		fprintf(stderr, "error: unrecognized argument\nrecognized arguments:\n--shell\n");
 		exit(1);
@@ -168,21 +230,12 @@ int main(int argc, char **argv) {
 	    close(pipeToChild[0]);
 	    close(pipeToParent[1]);
 
-	    readWriteEcho(0, 1);
-
-	    /*
-	    char buffer[2048];
-	    int count = 0;
-	    count = read(STDIN_FILENO, buffer, 2048);
-	    write(pipeToChild[1], buffer, count);
-	    count = read(pipeToParent[0], buffer, 2048);
-	    write(STDOUT_FILENO, buffer, count);
-	    */
+	    readWriteShell(0, 1);
 	}
     }
 
-    // readAndWrite(0, 1);
+    // if no shell flag, just do usual readWrite from stdin to stdout
+    readWrite(0, 1);
 
     exit(0);
-
 }
