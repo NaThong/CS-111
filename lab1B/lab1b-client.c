59191a1ae05fff75c68d1f46d04b2a713355fd47
Jeffrey Chan <jeffschan97@gmail.com>
2017-04-23 19:41:51 -0700

    getopt code

--- lab1B/lab1b-client.c
+++ lab1B/lab1b-client.c
@@ -1,5 +1,25 @@
 #include<stdio.h>
+#include<getopt.h>
+#include<stdlib.h>
+#include<unistd.h>
 
 int main(int argc, char *argv[]) {
-    printf("hello, world!\n");
+    int option = 0; // used to hold option
+
+    // arguments that this program recognizes
+    static struct option options[] = {
+    	{"port", required_argument, 0, 'p'},
+        {0, 0, 0, 0}
+    };
+
+    while ((option = getopt_long(argc, argv, "p", options, NULL)) != -1) {
+        switch (option) {
+            case 'p':
+                printf("received port option\n");
+                break;
+            default:
+                fprintf(stderr, "error: unrecognized argument\nrecognized arguments:\n--port\n");
+                exit(1);
+        }
+    }
 }
