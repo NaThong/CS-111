ac6eb1ab5f157bec3c94bd36c3618872199384eb
Jeffrey Chan <jeffschan97@gmail.com>
2017-04-23 20:35:08 -0700

    non-canonical input mode with no echo

--- lab1B/lab1b-client.c
+++ lab1B/lab1b-client.c
@@ -6,6 +6,42 @@
 #include<getopt.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<termios.h>
+#include<poll.h>
+
+// struct to hold saved terminal attributes
+struct termios savedAttributes;
+
+int portFlag = 0; // flag for port option
+
+
+void resetInputMode() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &savedAttributes);
+}
+
+void setInputMode() {
+    struct termios terminalAttributes;
+
+    // check if stdin is a terminal
+    if (!isatty(STDIN_FILENO)) {
+        fprintf(stderr, "stdin not a terminal.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // save terminal attributes
+    tcgetattr(STDIN_FILENO, &savedAttributes);
+    atexit(resetInputMode);
+
+    // set terminal mode
+    tcgetattr(STDIN_FILENO, &terminalAttributes);
+    terminalAttributes.c_lflag &= ~(ICANON|ECHO); // clear ICANON and ECHO
+    terminalAttributes.c_cc[VMIN] = 1;
+    terminalAttributes.c_cc[VTIME] = 0;
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &terminalAttributes) < 0) {
+        fprintf(stderr, "error: error in setting new termianl attributes");
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main(int argc, char *argv[]) {
     int option = 0; // used to hold option
@@ -18,10 +54,12 @@ int main(int argc, char *argv[]) {
         {0, 0, 0, 0}
     };
 
+    // parse through arguments
     while ((option = getopt_long(argc, argv, "p", options, NULL)) != -1) {
         switch (option) {
             case 'p':
                 printf("received port option\n");
+                portFlag = 1;
                 break;
             case 'l':
                 printf("received log option\n");
@@ -34,6 +72,15 @@ int main(int argc, char *argv[]) {
                 exit(1);
         }
     }
+
+    // if no port specified, exit
+    if (!portFlag) {
+        fprintf(stderr, "error: no port specified\n");
+        exit(1);
+    }
+
+    setInputMode();
+
     char buffer;
     while(read(0, &buffer, sizeof(char))) {
         write(1, &buffer, sizeof(char));
