c171c98b19bfd2cf931522053c881c3729ee1e3f
Jeffrey Chan <jeffreyc@lnxsrv09.seas.ucla.edu>
2017-05-08 22:11:25 -0700

    implemented all list features

---
+++ lab2A/SortedList.c
@@ -0,0 +1,87 @@
+// NAME: Jeffrey Chan
+// EMAIL: jeffschan97@gmail.com
+// ID: 004-611-638
+
+#include"SortedList.h"
+#include<stdlib.h>
+#include<string.h>
+#include<pthread.h>
+
+void SortedList_insert(SortedList_t *list, SortedListElement_t *element) {
+    if (list == NULL || element == NULL) { return; } // invalid arguments
+    SortedListElement_t *prev = list; // prev will point to list element just before curr
+    SortedListElement_t *curr = list->next; // set curr to first list element
+
+    while (curr != NULL) {
+	// point curr to first element that has a key greater than element's key
+	if (strcmp(element->key, curr->key) <= 0) break;
+
+	// move curr nad prev to next node
+	curr = curr->next;
+	prev = prev->next;
+    }
+
+    if (opt_yield & INSERT_YIELD) pthread_yield(); // yield if needed
+
+    // pointer reconnection to insert element into list
+    prev->next = element;
+    element->prev = prev;
+    element->next = curr;
+    if (curr != NULL) curr->prev = element;
+}
+
+int SortedList_delete(SortedListElement_t *element) {
+    if (element == NULL) { return 1; } // invalid arguments
+
+    // check that pointers aren't corrupted
+    if (element->prev->next == element) {
+	// case: last element in list
+	if (element->next == NULL) {
+	    if (opt_yield & DELETE_YIELD) pthread_yield(); // yield if necessary
+
+	    element->prev->next = NULL; // detach last element in list
+	    return 0;
+	}
+	// case: element inbetween to other valid elements
+	else if (element->next->prev == element) {
+	    if (opt_yield & DELETE_YIELD) pthread_yield(); // yield if necessary
+
+	    // detach element from list
+	    element->prev->next = element->next;
+	    element->next->prev = element->prev;
+	    return 0;
+	}
+    }
+    
+    return 1; // pointers corrupted and insertion failed
+}
+
+SortedListElement_t* SortedList_lookup(SortedList_t* list, const char *key) {
+    if (list == NULL || key == NULL) { return NULL; } // invalid argument
+    SortedListElement_t* curr = list->next; // declare list iterator
+
+    while (curr != NULL ) {
+	if (strcmp(curr->key, key) == 0) return curr; // found element
+
+	if (opt_yield & LOOKUP_YIELD) pthread_yield(); // yield if needed
+	
+	curr = curr->next; // move onto next element
+    }
+    
+    return NULL; // coun't find element
+}
+
+int SortedList_length(SortedList_t *list) {
+    if (list == NULL) return -1; // invalid arguments
+
+    int counter = 0;
+    SortedListElement_t* curr = list->next;
+
+    while (curr != NULL) {
+	counter++;
+	if (opt_yield & LOOKUP_YIELD) pthread_yield();
+	curr = curr->next;
+    }
+    
+    return counter;
+}
--- lab2A/lab2_list.c
+++ lab2A/lab2_list.c
@@ -1,10 +1,22 @@
+// NAME: Jeffrey Chan
+// EMAIL: jeffschan97@gmail.com
+// ID: 004-611-638
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<getopt.h>
+#include"SortedList.h"
+#include<pthread.h>
+#include<string.h>
+#include<time.h>
+
+#define BILL 1000000000L
 
 // global variables
 int numThreads = 1;
 int numIterations = 1;
+int opt_yield = 0;
+pthread_mutex_t mutex;
 
 int main(int argc, char **argv) {
     int option = 0; // used to hold option
@@ -14,7 +26,7 @@ int main(int argc, char **argv) {
         {"threads", required_argument, 0, 't'},
         {"iterations", required_argument, 0, 'i'},
         {"yield", required_argument, 0, 'y'}
-    }
+    };
 
     // iterate through options
     while ((option = getopt_long(argc, argv, "t:i:y", options, NULL)) != -1) {
@@ -25,6 +37,8 @@ int main(int argc, char **argv) {
             case 'i':
                 numIterations = atoi(optarg);
                 break;
+	    case 'y':
+		break; // TODO: implement yield option
             default:
                 fprintf(stderr, "error: unrecognized argument\nreocgnized arguments:\n--threads=#\n--iterations=#\n--yield=[idl]\n");
                 exit(1);
