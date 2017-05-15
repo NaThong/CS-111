8a3cd3ae33d4740f2126892d5628013564ecbe57
Jeffrey Chan <jeffreyc@lnxsrv09.seas.ucla.edu>
2017-05-15 00:21:25 -0700

    using new locking scheme

--- lab2B/lab2_list.c
+++ lab2B/lab2_list.c
@@ -88,35 +88,63 @@ void* listOperations(void* threadIndex) {
     int k;
     int totalElements = numThreads * numIterations;
 
-    if (syncOption == 'm') { pthread_mutex_lock(&mutex); }
-    else if (syncOption == 's') { while (__sync_lock_test_and_set(&spinCondition, 1)); }
-
     for (k= *(int*)threadIndex; k < totalElements; k += numThreads) {
-        SortedList_insert(list, &elementList[k]);
+	switch (syncOption) {
+	    case 'm':
+		pthread_mutex_lock(&mutex);
+		SortedList_insert(list, &elementList[k]);
+		pthread_mutex_unlock(&mutex);
+		break;
+	    case 's':
+		while(__sync_lock_test_and_set(&spinCondition, 1));
+		SortedList_insert(list, &elementList[k]);
+		__sync_lock_release(&spinCondition);
+		break;
+	    default:
+		SortedList_insert(list, &elementList[k]);
+		break;
+	}
     }
 
-    if (SortedList_length(list) == -1) {
-        fprintf(stderr, "error: failed to get length of list\n");
-        exit(2);
+    switch (syncOption) {
+	case 'm':
+	    pthread_mutex_lock(&mutex);
+    	    if (SortedList_length(list) == -1) {
+            	fprintf(stderr, "error: failed to get length of list\n");
+            	exit(2);
+    	    }
+	    pthread_mutex_unlock(&mutex);
+	    break;
+	case 's':
+	    while (__sync_lock_test_and_set(&spinCondition, 1));
+	    if (SortedList_length(list) == -1) {
+		fprintf(stderr, "error: failed to get length of list\n");
+		exit(2);
+	    }
+	    __sync_lock_release(&spinCondition);
+	    break;
+	default:
+	    if (SortedList_length(list) == -1) {
+		fprintf(stderr, "error: failed to get length of list\n");
+		exit(2);
+	    }
+	    break;
     }
 
     // lookup and delete previously inserted elements
     SortedListElement_t *temp = NULL;
     for (k = *(int*)threadIndex; k < totalElements; k += numThreads) {
         temp = SortedList_lookup(list, elementList[k].key);
-		if (temp == NULL) {
-		    fprintf(stderr, "error: failed to find element we already inserted\n");
-		    exit(2);
-		}
+	if (temp == NULL) {
+	    fprintf(stderr, "error: failed to find element we already inserted\n");
+	    exit(2);
+	}
         // if list corrupted, log error and exit with status
         if (SortedList_delete(temp)) {
             fprintf(stderr, "error: failed to delete an element we already inserted\n");
             exit(2);
         }
     }
-
-    if (syncOption == 'm') { pthread_mutex_unlock(&mutex); }
-    else if (syncOption == 's') { __sync_lock_release(&spinCondition); }
 
     return NULL;
 }
