a81034b56aa4ce35ee83e6b9d6d422e859e68b62
Jeffrey Chan <jeffschan97@gmail.com>
2017-05-20 22:32:11 -0700

    added temperature calculation, getting time, and writing to stdout

--- lab4B/lab4b.c
+++ lab4B/lab4b.c
@@ -9,11 +9,29 @@
 #include<string.h>
 #include<mraa.h>
 #include<mraa/aio.h>
+#include<sys/time.h>
+#include<time.h>
+#include<math.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
 
 // GLOBAL VARIABLES
-int period = 0;
+const int B = 4275; // value of thermistor
+int period = 1;
 char scale = 'F';
 char *logFile = NULL;
+int logFD = 0;
+
+double getTemperature(int rawTemperature, char scale) {
+	double temp = 1023.0 / ((double)rawTemperature) - 1.0;
+	temp = 100000.0 * temp;
+	double celsius = 1.0 / (log(temp/100000.0) / B + 1/298.15) - 273.15;
+
+	if (scale == 'C')
+		return celsius
+	return celsius * 9/5 + 32; // farenheit
+}
 
 int main(int argc, char **argv) {
 	int option = 0; // used to hold option
@@ -50,8 +68,38 @@ int main(int argc, char **argv) {
 	mraa_aio_context temperatureSensor;
 	temperatureSensor = mraa_aio_init(0);
 
+	// get log file descriptor
+	if (logFile) {
+		logFD = creat(logFile, 0666);
+		if (logFD < 0) {
+			fprintf(stderr, "error: unable to open log file");
+			exit(0);
+		}
+	}
+
 	// to hold raw temperature read by temperature sensor
 	int rawTemperature = 0;
+
+	// initialize time variables
+	time_t timer;
+	char timeString[10];
+	struct tm* timeInfo;
+
+	while (1) {
+		// get temperature
+		rawTemperature = mraa_aio_read(temperatureSensor);
+		double processedTemperature = getTemperature(rawTemperature, scale);
+
+		// get current time
+		time(&timer);
+		timeInfo = localTime(&timer);
+		strftime(timeBuffer, 10, "%H:%M:%S", timeInfo);
+
+		// print to stdout and log file
+		fprintf(stdout, "%s %.1f\n", timeBuffer, processedTemperature);
+
+		sleep(1 / period);
+	}
 
 	exit(0);
 }
