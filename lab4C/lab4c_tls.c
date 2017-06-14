0a56ff663c1111e0dd4a85254c15317a71060f3e
Jeffrey Chan <jeffschan97@gmail.com>
2017-06-14 08:02:11 -0700

    pls

--- lab4C/lab4c_tls.c
+++ lab4C/lab4c_tls.c
@@ -258,37 +258,43 @@ int main(int argc, char **argv) {
 			if (mraa_gpio_read(button))
 				handleShutdown(logFile);
 
-			// poll for input
-			int returnValue = poll(pollfdArray, 1, 0);
-			if (returnValue < 0) {
-				fprintf(stderr, "error: error while polling\n");
-				exit(1);
-			}
+		// 	// poll for input
+		// 	int returnValue = poll(pollfdArray, 1, 0);
+		// 	if (returnValue < 0) {
+		// 		fprintf(stderr, "error: error while polling\n");
+		// 		exit(1);
+		// 	}
+        //
+		// 	if ((pollfdArray[0].revents & POLLIN)) {
+        //         //
+        //         // FILE* fdf = fdopen(socketFD, "r");
+     //  	// 		char commBuff[1024];
+    	// 		// char c;
+    	// 		// int buffIndex = 0;
+        //         // while (1) {
+    	// 		// 	if(read(socketFD, &c, 1) > 0) {
+    	// 		// 		if (c == '\n') {
+    	// 		// 			commBuff[buffIndex] = '\0';
+    	// 		// 			buffIndex = 0;
+    	// 		// 			break;
+    	// 		// 		}
+    	// 		// 		commBuff[buffIndex] = c;
+    	// 		// 		buffIndex++;
+    	// 		// 	}
+    	// 		// }
+        //
+        //         char commBuff[1024];
+        //         memset(commBuff, 0, 1024);
+        //         int charsRead = SSL_read(ssl, commBuff, 1024);
+        //         fprintf(stdout, "command: %s\n", commBuff);
+        //         handleCommand(commBuff);
+		// 	}
 
-			if ((pollfdArray[0].revents & POLLIN)) {
-                //
-                // FILE* fdf = fdopen(socketFD, "r");
-      	// 		char commBuff[1024];
-    			// char c;
-    			// int buffIndex = 0;
-                // while (1) {
-    			// 	if(read(socketFD, &c, 1) > 0) {
-    			// 		if (c == '\n') {
-    			// 			commBuff[buffIndex] = '\0';
-    			// 			buffIndex = 0;
-    			// 			break;
-    			// 		}
-    			// 		commBuff[buffIndex] = c;
-    			// 		buffIndex++;
-    			// 	}
-    			// }
-
-                char commBuff[1024];
-                memset(commBuff, 0, 1024);
-                int charsRead = SSL_read(ssl, commBuff, 1024);
-                fprintf(stdout, "command: %s\n", commBuff);
-                handleCommand(commBuff);
-			}
+            char commBuff[1024];
+            memset(commBuff, 0, 1024);
+            int charsRead = SSL_read(ssl, commBuff, 1024);
+            fprintf(stdout, "command: %s", commBuff);
+            handleCommand(commBuff);
 
             // sample new ending time
 			if (run)
