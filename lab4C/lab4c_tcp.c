8205a0ffebf02c7554da2f9e6b84536fcc08049a
Jeffrey Chan <jeffschan97@gmail.com>
2017-06-13 21:25:15 -0700

    pls

--- lab4C/lab4c_tcp.c
+++ lab4C/lab4c_tcp.c
@@ -210,7 +210,7 @@ int main(int argc, char **argv) {
 
 		// print to stdout and log file
         fprintf(stdout, "%s %.1f\n", timeString, processedTemperature);
-		fprintf(socketFD, "%s %.1f\n", timeString, processedTemperature);
+		dprintf(socketFD, "%s %.1f\n", timeString, processedTemperature);
 		if (logFile) {
 			fprintf(logFile, "%s %.1f\n", timeString, processedTemperature);
 		}
@@ -233,7 +233,7 @@ int main(int argc, char **argv) {
 			if ((pollfdArray[0].revents & POLLIN)) {
 				char command[50];
 				scanf("%s", command);
-                // fprintf(stdout, "command: %s\n", command);
+                dprintf(socketFD, "command: %s\n", command);
 				handleCommand(command);
 			}
 			if (run)
